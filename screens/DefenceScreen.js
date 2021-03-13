78376e5cf6dd9559b839c9e2f5ae5836edd65b5b
Ashwika071106-rgb <72143078+Ashwika071106-rgb@users.noreply.github.com>
2021-03-13 06:35:20 +0000

    Add files via upload

--- screens/DefenceScreen.js
+++ screens/DefenceScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Text,View,StyleSheet} from 'react-native';
+import Video from 'react-native-video';
 
 export default class DefenceScreen extends React.Component{
     render(){
@@ -7,6 +8,24 @@ export default class DefenceScreen extends React.Component{
             <View style = {styles.container}>
                 <View style = {styles.header}>
                     <Text style = {styles.headerText}>Defend Yourself</Text>
+                </View>
+                <View>
+                    <View>
+                        <Text>
+                            According to wikipedia, Self - defense is a resistant material that includes the protection of health and the well-being of harming oneself. The use of the right to self-defense as a legal tiff deed for the use of force in times of danger is available in many jurisdictions.
+                        </Text>
+                        <Text>
+                            In simple words, self defence is the ability to protect yourself in a dangerous situation.
+                        </Text>
+                    </View>
+                        <Text>WAYS TO DEFEND YOURSELF</Text>
+                        <Video source={{uri: "https://www.youtube.com/watch?v=T7aNSRoDCmg"}}  
+                            ref={(ref) => {
+                                this.player = ref
+                            }}                                      
+                            onBuffer={this.onBuffer}                
+                            onError={this.videoError}
+                        />
                 </View>
             </View>
         )
